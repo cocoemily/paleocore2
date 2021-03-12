19e98bd0ffe10c9e2b87e416f9b9cf2c46854abb
dennereed <denne.reed@gmail.com>
2021-03-12 12:21:45 -0800

    Add ICZN type definitions as choice list

--- origins/ontologies.py
+++ origins/ontologies.py
@@ -30,6 +30,43 @@ CONTINENT_CHOICES = (
     (south_america, 'South America')
 )
 
+# Type Specimens Choices
+# Definitions copied from ICZN online http://code.iczn.org
+allotype = 'allotype' # A term, not regulated by the Code, for a designated specimen of opposite sex to the holotype
+cotype = 'cotype' # A term not recognized by the Code, formerly used for either syntype or paratype, but that should
+# not now be used in zoological nomenclature
+genotype = 'genotype' # A term not recognized by the Code, formerly used for type species, but that should not
+# now be used in zoological nomenclature
+hapanotype = 'hapanotype' # One or more preparations consisting of directly related individuals representing distinct
+# stages in the life cycle, which together form the name-bearing type in an extant species of protistan.
+holotype = 'holotype'  # The single specimen (except in the case of a hapantotype, q.v.) designated or otherwise fixed
+# as the name-bearing type of a nominal species or subspecies when the nominal taxon is established.
+isotype = 'isotype' # A duplicate specimen of the holotype.
+isosyntype = 'isosyntype'  # A duplicate of a syntype.
+paratype = 'paratype' # A specimen not formally designated as a type but cited along with the type collection in the
+# original description of a taxon.
+lectotype = 'lectotype' # A syntype designated as the single name-bearing type specimen subsequent to the establishment
+# of a nominal species or subspecies
+neotype = 'neotype' # The single specimen designated as the name-bearing type of a nominal species or subspecies
+# when there is a need to define the nominal taxon objectively and no name-bearing type is believed to be extant.
+# If stability and universality are threatened, because an existing name-bearing type is either taxonomically
+# inadequate or not in accord with the prevailing usage of a name, the Commission may use its plenary power
+# to set aside that type and designate a neotype.
+paralectotype = 'paralectotype' # Each specimen of a former syntype series remaining after the designation
+# of a lectotype
+syntype = 'syntype'  # Each specimen of a type series (q.v.) from which neither a holotype nor a lectotype has
+# been designated. The syntypes collectively constitute the name-bearing type.
+topotype = 'topotype'  # A term, not regulated by the Code, for a specimen originating from the type locality of the
+# species or subspecies to which it is thought to belong, whether or not the specimen is part of the type series.
+
+TYPE_CHOICES = (
+    (holotype, 'holotype'),
+    (paratype, 'paratype'),
+    (lectotype, 'lectotype'),
+    (neotype, 'neotype'),
+    (syntype, 'syntype'),
+)
+
 
 # helper functions
 def choices2list(choices_tuple):
