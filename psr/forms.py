f1f2f848cde0b79b49eb635c77e9afb5e739b98d
Emily Coco <ec3307@nyu.edu>
2021-07-08 14:04:14 -0400

    updated functions for data collection forms

--- psr/forms.py
+++ psr/forms.py
@@ -29,12 +29,14 @@ class UploadShapefileDirectory(forms.Form):
 class UploadMDB(forms.Form):
     mdbUpload = forms.FileField(
         label='Upload an Access Database, *.mdb',
+        widget=forms.FileInput(attrs={'accept': 'application/mdb'}) #not actually a file type, so doesn't limit
     )
 
 
 class UploadJSON(forms.Form):
     jsonUpload = forms.FileField(
         label='Upload a JSON file, *.json',
+        widget=forms.FileInput(attrs={'accept':'application/json'})
     )
 
 
