0ff73e58bff577edbf9db4408d684a53a0c4edfa
dennereed <denne.reed@gmail.com>
2021-07-18 11:37:13 -0700

    Python 3 updates

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals, absolute_import
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from django_countries.fields import CountryField
