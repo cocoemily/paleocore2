2af85c031c01a64bf40bd53a130dcaac3d9a22cf
dennereed <denne.reed@gmail.com>
2021-12-09 08:26:19 -0600

    add potentially valid boolean to Nomen model

--- origins/admin.py
+++ origins/admin.py
@@ -566,6 +566,7 @@ class TurkanaFossilAdmin(admin.ModelAdmin):
 class TurkFossilAdmin(admin.ModelAdmin):
     list_display = ['catalog_number', 'verbatim_suffix', 'region',
                     'suffix_assigned', 'in_origins', 'to_add', 'to_divide', 'origins_fossil']
+    list_editable = ['to_add', 'to_divide']
     list_filter = ['region', 'suffix_assigned', 'in_origins']
     search_fields = ['verbatim_inventory_number', 'verbatim_suffix', 'catalog_number']
     readonly_fields = ['origins_fossil']
