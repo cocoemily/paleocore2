d4bdb7cc9256d962fcaca3b73132a86131cb44a1
dennereed <denne.reed@gmail.com>
2022-05-25 15:14:05 -0700

    Add locality model

--- inca/admin.py
+++ inca/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Specimen
-from projects.admin import PaleoCoreOccurrenceAdmin
+from .models import Specimen, Locality
+from projects.admin import PaleoCoreOccurrenceAdmin, PaleoCoreLocalityAdminGoogle
+
+
+@admin.register(Locality)
+class LocalityAdmin(PaleoCoreLocalityAdminGoogle):
+    list_display = ['locality_id', 'collection_code', 'name',
+                    'earliest_age_ma', 'latest_age_ma', 'age_protocol', 'year_established',
+                    'archaeological_evidence', 'archaeology_present',
+                    'macrobotanical_evidence', 'macrobotanical_present',
+                    ]
+    list_filter = ['collection_code', 'year_established', 'archaeology_present',
+                   'macrobotanical_present', 'uncollected_fossils_present']
+    search_fields = ['locality_id', 'name', 'bed']
 
 
 @admin.register(Specimen)
--- inca/models.py
+++ inca/models.py
@@ -7,6 +7,46 @@ class Person(projects.models.PaleoCoreBaseClass):
     given_name = models.CharField(max_length=255, blank=True, null=True)
 
 
+class Locality(projects.models.PaleoCoreLocalityBaseClass):
+    """
+    Locality Class <- PaleoCoreLocalityBaseClass <- PaleoCoreGeomBaseClass
+    """
+    locality_id = models.CharField(max_length=255, blank=True, null=True)
+    collection_code = models.CharField(max_length=10, null=True, blank=True)
+    #name: inheritied from base class
+    formation = models.CharField(max_length=255, blank=True, null=True)
+    member = models.CharField(max_length=255, blank=True, null=True)
+    bed = models.TextField(null=True, blank=True)
+    verbatim_age_ma = models.CharField(max_length=255, blank=True, null=True)
+    earliest_age_ma = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True)
+    latest_age_ma = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True)
+    age_protocol = models.CharField(max_length=255, blank=True, null=True)
+    year_established = models.IntegerField(null=True, blank=True)
+    dimension_ew = models.CharField(max_length=255, blank=True, null=True)
+    dimension_ns = models.CharField(max_length=255, blank=True, null=True)
+    locality_description = models.TextField(null=True, blank=True)
+    locality_boundary_east = models.CharField(max_length=255, blank=True, null=True)
+    locality_boundary_north = models.CharField(max_length=255, blank=True, null=True)
+    locality_boundary_south = models.CharField(max_length=255, blank=True, null=True)
+    locality_boundary_west = models.CharField(max_length=255, blank=True, null=True)
+    macrobotanical_evidence = models.CharField(max_length=255, blank=True, null=True)
+    archaeological_evidence = models.CharField(max_length=255, blank=True, null=True)
+    uncollected_taxa = models.CharField(max_length=255, blank=True, null=True)
+    archaeology_present = models.BooleanField(null=True, blank=True)
+    macrobotanical_present = models.BooleanField(null=True, blank=True)
+    elevation = models.IntegerField(null=True, blank=True)
+    georeference_protocol = models.CharField(max_length=255, blank=True, null=True)
+    verbatim_row_data = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Locality'
+        verbose_name_plural = 'Localities'
+        ordering = ['locality_id']
+
+    def __str__(self):
+        return f"[{self.id}] {self.locality_id}"
+
+
 class Specimen(projects.models.PaleoCoreOccurrenceBaseClass):
     """
     Specimen <- PaleoCoreOccurrenceBaseClass <- PaleoCoreGeomBaseClass <- PaleoCoreBaseClass
