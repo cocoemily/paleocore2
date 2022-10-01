647da0cd1cdcd027eddb6a2c89d6e46de19095eb
dennereed <denne.reed@gmail.com>
2022-10-01 09:00:43 -0500

    add type_specimen_status to Nomen model

--- origins/models/taxon.py
+++ origins/models/taxon.py
@@ -97,6 +97,7 @@ class Nomen(projects.models.PaleoCoreBaseClass):
     type_specimen_label = models.CharField(max_length=255, null=True, blank=True, help_text=type_help)
     type_specimen = models.ForeignKey('Fossil', null=True, blank=True, on_delete=models.SET_NULL,
                                       help_text=type_object_help)
+    type_specimen_status = models.CharField(max_length=255, null=True, blank=True, choices=TYPE_CHOICES)
     paratypes = models.TextField(null=True, blank=True)
     type_taxon = models.CharField(max_length=255, null=True, blank=True)
 
