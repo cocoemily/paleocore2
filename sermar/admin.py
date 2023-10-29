0e3fab7619850af09388a2ce8ffa491f21785955
dennereed <denne.reed@gmail.com>
2023-10-29 09:23:23 -0500

    Add morphotypes data and associated models and admin.

--- sermar/admin.py
+++ sermar/admin.py
@@ -9,7 +9,7 @@ from import_export.admin import ImportExportModelAdmin
 from mapwidgets.widgets import GooglePointFieldWidget
 
 # App imports
-from sermar.models import Locality, Collection, Biology
+from sermar.models import Locality, Collection, Biology, Morphotype
 
 
 # Admin Classes
@@ -69,7 +69,8 @@ class CollectionResource(resources.ModelResource):
 
 
 class CollectionAdmin(ImportExportModelAdmin):
-    list_display = ['collection_code', 'locality', 'date', 'disposition', 'specimen_loc', 'sample_size',
+    readonly_fields = ['label']
+    list_display = ['name','label', 'collection_code', 'locality', 'date', 'disposition', 'specimen_loc', 'sample_size',
                     'source', 'status', 'bags']
     list_filter = ['source', 'specimen_loc', 'locality']
     search_fields = ['collection_code', 'locality__name', 'locality__roost_id']
@@ -84,14 +85,111 @@ class BiologyResource(resources.ModelResource):
 
 
 class BiologyAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'barcode', 'collection', 'morphotype_id', 'element_id',  'loan', 'disposition']
-    list_filter = ['collection_code', 'loan', 'morphotype_id']
-    search_fields = ['id', 'barcode', 'item_number',  'catalog_number']
+    list_display = ['barcode', 'collection', 'morphotype', 'element_id',  'loan', 'disposition', 'default_image']
+    list_filter = ['collection_code', 'loan', 'verbatim_morphotype_id']
+    search_fields = ['barcode', 'item_number',  'catalog_number']
+    readonly_fields = ['default_image']
+    fieldsets = [
+        ('Record Details', {
+            'fields': [
+                ('barcode', 'field_number_orig'),
+                ('morphobank_number',),
+                ('basis_of_record', 'item_type'),
+                ('collection',),
+                ('collection_code', 'item_number', 'item_part'),
+                ('related_catalog_items',),
+                ('collector', 'finder'),
+                ('loan', 'loan_date'),
+                ('disposition',),
+                ('individual_count',),
+                ('preparation_status',),
+
+            ]
+        }),
+        ('Identification', {
+            'fields': [
+                ('verbatim_morphotype_id', 'morphotype'),
+                ('item_scientific_name', 'author_year_of_scientific_name'),
+                ('infraspecific_epithet', 'infraspecific_rank'),
+                ('nomenclatural_code',),
+                ('identified_by', 'date_identified'),
+                ('type_status',)
+            ]
+        }),
+        ('Biology Details', {
+            'fields': [
+                ('sex',),
+                ('life_stage',),
+            ]
+        }),
+        ('Preservation', {
+            'fields': [
+                ('item_description',),
+                ('side',),
+                ('attributes',),
+                ('collecting_method',),
+                ('preparations',),
+                ('in_situ',),
+                ('weathering', 'surface_modification')
+            ]
+        }),
+        ('Anatomical Elements', {
+            'fields': [
+                ('tooth_upper_or_lower',),
+                ('tooth_number',),
+                ('tooth_type',),
+                ('element_id','element', 'element_modifier'),
+                ('lli1', 'lri1', 'uri1', 'uli1'),
+                ('lli2', 'lri2', 'uri2', 'uli2'),
+                ('lli3', 'lri3', 'uri3', 'uli3'),
+                ('lli4', 'lri4', 'uri4', 'uli4'),
+                ('lli5', 'lri5', 'uri5', 'uli5'),
+                ('llc', 'lrc', 'urc', 'ulc'),
+                ('llp1', 'lrp1', 'urp1', 'ulp1'),
+                ('llp2', 'lrp2', 'urp2', 'ulp2'),
+                ('llp3', 'lrp3', 'urp3', 'ulp3'),
+                ('llp4', 'lrp4', 'urp4', 'ulp4'),
+                ('llm1', 'lrm1', 'urm1', 'ulm1'),
+                ('llm2', 'lrm2', 'urm2', 'ulm2'),
+                ('llm3', 'lrm3', 'urm3', 'ulm3'),
+
+            ]
+        }),
+        ('Measurements', {
+            'fields': [
+                ('lm_tooth_row_length_mm',),
+                ('lm_1_length', 'lm_1_width'),
+                ('lm_2_length', 'lm_2_width'),
+                ('lm_3_length', 'lm_3_width'),
+                ('um_tooth_row_length_mm',),
+                ('um_1_length_mm', 'um_1_width_mm'),
+                ('um_2_length_mm', 'um_2_width_mm'),
+                ('um_3_length_mm', 'um_3_width_mm'),
+            ]
+        }),
+        ('Remarks', {
+            'fields': [
+                ('fauna_notes',),
+                ('remarks',),
+                ('problem',),
+                ('problem_comment',),
+                ('date_created', 'date_last_modified'),
+
+            ]
+        }),
+
+    ]
 
     resource_class = BiologyResource
 
+class MorphotypeAdmin(admin.ModelAdmin):
+    list_display = ['morph_id', 'name', 'order_id', 'species1_id', 'species2_id', 'species3_id', 'species4_id',
+                    'analysis_id']
+    list_filter = ['order_id']
+    search_fields = ['name', 'morph_id']
 
 # Admin Registration
 admin.site.register(Biology, BiologyAdmin)
 admin.site.register(Locality, LocalityAdmin)
 admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Morphotype, MorphotypeAdmin)
